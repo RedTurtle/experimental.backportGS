86e12ecbefe27204b4505e42ce407d1985b6910c
Luca Fabbri <luca@keul.it>
2012-12-12 10:23:47 +0100

    moved to .rst documentation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1'
 setup(name='experimental.backportGS',
       version=version,
       description="Monkey-patch Plone 3 to make Plone 4 Generic Setup import steps to continue working",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
